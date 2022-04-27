2cbe13f64c0e4d94259351650036d8679168ae59
choewy <choewy32@gmail.com>
2022-04-27 17:20:38 +0900

    [테스트] 우분투 환경으로 변경

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,18 +1,6 @@
-# !/bin/bash
+#!/bin/bash
 
-# download nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-source .bash_profile
-
-# install node, npm
-nvm install v9.4.0
-nvm use v9.4.0
-
-# install pm2
-npm install -g pm2
-
-# create our working directory if it doesnt exist
-DIR="/home/ec2-user/express-app"
+DIR="/home/ubuntu/build"
 if [ -d "$DIR" ]; then
   echo "${DIR} exists"
 else
--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/ubuntu/build/
-npm run delete
+npm install

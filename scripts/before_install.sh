430f5f1037d8ed989976bae425722aff08cd8f66
choewy <choewy32@gmail.com>
2022-04-27 13:36:57 +0900

    [수정] before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,9 +1,15 @@
 # !/bin/bash
 
-# download node and npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
+# download nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+source .bash_profile
+
+# install node, npm
+nvm install v9.4.0
+nvm use v9.4.0
+
+# install pm2
+npm install -g pm2
 
 # create our working directory if it doesnt exist
 DIR="/home/ec2-user/express-app"

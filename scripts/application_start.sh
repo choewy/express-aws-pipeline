11ca826db6c568e6ce9fa574f7f2083c6a2b2605
choewy <choewy32@gmail.com>
2022-04-27 23:56:06 +0900

    [수정] 다시 CodeDeploy + Pipeline 구축

---
+++ scripts/application_start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /home/ubuntu/express-server
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+npm install
+npm run start

999107e21dcc50b9ac320b5d02d9f448073a8666
choewy <choewy32@gmail.com>
2022-04-27 12:39:02 +0900

    [초기화] 프로젝트 초기화

---
+++ README.md
@@ -0,0 +1,7 @@
+# EXPRESS AWS CI/CD 구축 테스트
+
+- TypeScript
+- express
+- pm2
+- AWS EC2
+- AWS Code Deploy
---
+++ src/bin/www.ts
@@ -0,0 +1,5 @@
+import app from '../app';
+
+app.listen(5000, () => {
+  console.log('Server running on port 5000');
+});
